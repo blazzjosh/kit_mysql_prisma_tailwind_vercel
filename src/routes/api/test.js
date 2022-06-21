a71d124dbba8978ab7357907aff6bb954eb3e31d
blazzjosh <blazzjosh@live.com>
2022-06-21 11:03:04 +0100

    deploy

--- src/routes/api/test.js
+++ src/routes/api/test.js
@@ -1,7 +1,7 @@
 import prisma from "$lib/prisma";
 
 
-export const get = async ({ clientAddress, locals, params, platform, request, routeId, url }) => {
+export const get = async () => {
     const test = await prisma.Todo.findMany()
     return {
       body: {
